b8a8aa59601aadd6f21b4a9895c8c5c489797ce3
carlosaj98 <carlosaj.arahal@gmail.com>
2024-08-23 19:46:06 +0200

    fix: initial deploy issues

--- app/src/pages/HomePage/HomePage.tsx
+++ app/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
-import Navbar from "../../components/Navbar/Navbar"
+// import Navbar from "../../components/Navbar/Navbar"
 import HomePageContainer from "./HomePage.style"
 import SectionMain from "./sections/SectionMain"
-import SectionServices from "./sections/SectionServices"
+// import SectionServices from "./sections/SectionServices"
 import { Container } from "@mui/material"
 
 const HomePage: React.FC = () => {
   return (
     <HomePageContainer sx={{ alignItems: "center" }}>
-      <Navbar />
+      {/* <Navbar /> */}
       <Container sx={{ height: "100%" }}>
         <SectionMain />
         {/* <SectionServices/> */}
